c036e6d8e552e7397a83c245087bc6ba5a19b528
thenatural86 <philip.smalls86@gmail.com>
2020-03-06 08:43:40 -0500

    wildcard navigation

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,11 +8,15 @@ class StreamList extends React.Component {
     this.props.fetchStreams()
   }
 
+  // render edit/delete buttons
   renderAdmin = stream => {
+    // if this stream belongs to the user that is currently logged in render buttons
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link to={`streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
           <button className="ui button negative">Delete</button>
         </div>
       )
