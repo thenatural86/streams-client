cfbd3b9e9e4cd8d6806fda50b1b032d4b646b78d
thenatural86 <philip.smalls86@gmail.com>
2020-03-06 08:21:57 -0500

    navigate programmatically via history object

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams"
+import history from "../history"
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -24,6 +25,7 @@ export const createStream = formValues => async (dispatch, getState) => {
 
   dispatch({ type: CREATE_STREAM, payload: response.data })
   // Do some programmatic navigation to get user back to root route
+  history.push("/")
 }
 
 export const fetchStreams = () => async dispatch => {
