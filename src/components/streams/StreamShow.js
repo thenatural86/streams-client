1d1278add78ac1febda0a45f1b8bc5bf621b3489
thenatural86 <philip.smalls86@gmail.com>
2020-03-12 09:04:06 -0400

    video streaming

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -6,11 +6,42 @@ import { fetchStream } from "../../actions/index"
 class StreamShow extends React.Component {
   constructor(props) {
     super(props)
-    this.videRef = React.createRef()
+
+    this.videoRef = React.createRef()
   }
 
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)
+    // destructuring out id property
+    const { id } = this.props.match.params
+    // console.log(this.videoRef)
+
+    this.props.fetchStream(id)
+    this.buildPlayer()
+  }
+
+  // component fetches stream successfully and re-renders componentDidUpdate gets called
+  componentDidUpdate() {
+    this.buildPlayer()
+  }
+
+  componentWillUnmount() {
+    this.player.destroy()
+    console.log("unmount")
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return
+    }
+
+    const { id } = this.props.match.params
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
   }
 
   render() {
@@ -20,7 +51,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream
     return (
       <div>
-        <video ref={this.videRef} style={{ width: "100%" }} controls />
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
