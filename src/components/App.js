760b5eee3b5be23bfd157e96b2234d842111b28a
thenatural86 <philip.smalls86@gmail.com>
2020-03-16 16:33:37 -0400

    router path

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,21 @@ import StreamDelete from "./streams/StreamDelete"
 import StreamList from "./streams/StreamList"
 import StreamShow from "./streams/StreamShow"
 import Header from "./Header"
+// keeps track of the address bar in browser
 import history from "../history"
 
 // Router allows navigation around app by matching the url path.
 const App = () => {
   return (
     <div className="ui container">
-      {/* give all pages access to history object */}
+      {/* Listens to history for changes in URL. Gives all pages access to history object */}
       <Router history={history}>
         <div>
           {/* Header will on top of all pages in app */}
           <Header />
           {/* Wrap all routes for app in Switch */}
           <Switch>
+            {/* path prop decides if it should so component in screen or not. */}
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit/:id" exact component={StreamEdit} />
