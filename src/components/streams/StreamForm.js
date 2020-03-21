75a4dfbde22a5e6202f39f6550aae2952b656e69
thenatural86 <philip.smalls86@gmail.com>
2020-03-21 16:01:39 -0400

    formProps de-structured

--- src/components/streams/StreamForm.js
+++ src/components/streams/StreamForm.js
@@ -1,4 +1,5 @@
 import React from "react"
+// redux form imports. Field is a react component, reduxForm is a function that has same functionality as connect that allows us to call an action creator and get form data into our component.
 import { Field, reduxForm } from "redux-form"
 
 class StreamForm extends React.Component {
@@ -12,11 +13,14 @@ class StreamForm extends React.Component {
     }
   }
 
+  // render input helper method
+  // input and meta de-structured out of formProps object from redux form
   renderInput = ({ input, label, meta }) => {
     const className = `field ${meta.error && meta.touched ? "error" : ""}`
     return (
       <div className={className}>
         <label>{label}</label>
+        {/* input properties from formProps added onto input element  */}
         <input {...input} autoComplete="off" />
         {this.renderError(meta)}
       </div>
@@ -34,6 +38,8 @@ class StreamForm extends React.Component {
         className="ui form error"
         onSubmit={this.props.handleSubmit(this.onSubmit)}
       >
+        {/* field component imported from redux-form */}
+        {/* needs a name and component prop. component can be a react component or a function for the Field to call */}
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
@@ -58,7 +64,9 @@ const validate = formValues => {
   return errors
 }
 
+// reduxForm receives an object
 export default reduxForm({
+  // name for form ("streamCreate") has it's values stored inside the form reducer
   form: "streamForm",
   validate
 })(StreamForm)
