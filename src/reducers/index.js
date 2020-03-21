d3b65e9c4be160c78429629be583af4ae76c2334
thenatural86 <philip.smalls86@gmail.com>
2020-03-21 13:33:48 -0400

    redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,15 @@
 import { combineReducers } from "redux"
+// alias reducer as formReducer
 import { reducer as formReducer } from "redux-form"
 import authReducer from "./authReducer"
 import streamReducer from "./streamReducer"
 
-//
+// combine all of the reducers
 export default combineReducers({
   // authReducer file assigned to key of auth
   auth: authReducer,
 
-  // formReducer file assigned to key of form
+  // formReducer file assigned to key of form from redux-form library
   form: formReducer,
 
   // streamsReducer file assigned to key of streams
